d1c1337568d8ff5a635120b52520736d3e685c84
David Cole <davidtcole@yahoo.com>
2020-06-08 21:44:01 -0700

    add the relevant information about the project to the README.md file

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
-  $("#form-submit").submit(function(event){
+  $("#grocery-form").submit(function(event){
     event.preventDefault();
+    const groceryList = $("#groceries").val();
+
+    if(groceryList) {
+      $("#grocery-form").toggle();
+      $("#display-items").toggle();
+      const groceryArray = groceryList.toUpperCase().split(", ").sort();
+      groceryArray.forEach(function(item) {
+        $("#list-items").append("<li>" + item + "</li>");
+      });
+    } else {
+      alert("Please enter some items to your grocery list.")
+    }
+
   });
 });
